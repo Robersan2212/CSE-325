2a06bac0956ebb2f484d9d9a8da997cf0d7df3df
Roberto Sanchez <san20086@byui.edu>
2026-01-17 01:19:51 -0700

    Add NuGet configuration files and project assets for first-C#-code
    - Created `first-C#-code.csproj.nuget.g.props` to define NuGet properties.
    - Created `first-C#-code.csproj.nuget.g.targets` for build targets.
    - Added `project.assets.json` to manage project dependencies and assets.
    - Introduced `project.nuget.cache` for caching NuGet package information.

---
+++ U01/first-C#-code/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//Console.WriteLine("Congratulations!");
+//Console.Write("You wrote your first line of code.");
+
+//Code challenge 1
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
